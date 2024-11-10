376cbe7a5649aaa20294d4d652fb70d207bf1461
James Blackman <83969784+JamesBlackman-SLG@users.noreply.github.com>
2024-11-10 21:12:37 +0000

    docker

--- consumer/main.go
+++ consumer/main.go
@@ -376,5 +376,5 @@ func main() {
 
 	http.HandleFunc("/webhook", webhookHandler(db))
 	log.Println("Starting server on :8443 with HTTPS...")
-	log.Fatal(http.ListenAndServeTLS(":8443", "/etc/letsencrypt/live/group800.silverlininggroup.co.uk/fullchain.pem", "/etc/letsencrypt/live/group800.silverlininggroup.co.uk/privkey.pem", nil))
+	log.Fatal(http.ListenAndServeTLS(":8443", "fullchain.pem", "privkey.pem", nil))
 }
