f10490167c71e7f80abbc58112075271587997f6
James Blackman <james@silverlininggroup.co.uk>
2024-12-04 14:57:52 +0000

    merge complete

--- web/internals/routes.go
+++ web/internals/routes.go
@@ -2,7 +2,6 @@ package internals
 
 import (
 	"database/sql"
-	"log"
 	"net/http"
 
 	"github.com/gin-contrib/sessions"
@@ -14,37 +13,6 @@ import (
 type Config struct {
 	Router *gin.Engine
 	DB     *sql.DB
-}
-
-func (app *Config) signInHandler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// Dummy authentication logic
-		username := c.PostForm("username")
-		password := c.PostForm("password")
-
-		if username == "admin" && password == "slg2024" {
-			log.Print("Login successful")
-			// Set a session to indicate successful login
-			session := sessions.Default(c)
-			session.Set("authenticated", true)
-			_ = session.Save()
-
-			c.Redirect(http.StatusFound, "/")
-		} else {
-			log.Print("Login failed")
-			c.Redirect(http.StatusFound, "/login?login=failed")
-			// c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid credentials"})
-		}
-	}
-}
-
-func (app *Config) signOutHandler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		session := sessions.Default(c)
-		session.Set("authenticated", false)
-		_ = session.Save()
-		c.Redirect(http.StatusFound, "/login")
-	}
 }
 
 func (app *Config) authMiddleware() gin.HandlerFunc {
@@ -64,16 +32,17 @@ func (app *Config) Routes() {
 	store := cookie.NewStore([]byte("secret"))
 	app.Router.Use(sessions.Sessions("mysession", store))
 
+	// Add logging middleware to debug static file serving
+	app.Router.Use(gin.Logger())
+
+	// Serve static files from the /views/static directory
+	app.Router.Static("/static", "../views/static")
 	app.Router.POST("/webhook", app.webHookHandler())
 
 	// login
 	app.Router.GET("/login", app.loginPageHandler())
 	app.Router.POST("/signin", app.signInHandler())
 	app.Router.GET("/signout", app.signOutHandler())
-	app.Router.GET("/logo", app.logoHandler())
-	app.Router.GET("/favicon/:f", app.faviconHandler())
-	app.Router.GET("/manifest.json", app.manifestJson())
-	app.Router.GET("/styles.css", app.handleStyles())
 
 	// Apply authentication middleware
 	app.Router.Use(app.authMiddleware())
